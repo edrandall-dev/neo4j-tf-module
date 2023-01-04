f3a1e1fa717863971f127b5ce456251d21a7c89b
edrandall-dev <ed.randall@neo4j.com>
2023-01-04 11:17:34 +0000

    updates to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "ssh_commands_for_each_instance" {
+output "ssh_commands" {
   value = [  
     for key, value in aws_instance.neo4j_instance[*].public_ip: "ssh -o StrictHostKeyChecking=no -i ${var.private_key_path} ec2-user@${value}"
   ]
