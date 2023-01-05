664d823718267777e467a6b430ed9a44fbe64756
edrandall-dev <ed@edrandall.uk>
2023-01-05 15:25:32 +0000

    outputs

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@ output "ssh_commands" {
     for key, value in aws_instance.neo4j_instance[*].public_ip: "ssh -o StrictHostKeyChecking=no -i ${var.private_key_path} ec2-user@${value}"
   ]
 }
+
+output "graphDataScienceLicenseKey" {
+  value = ${var.graph_data_science_license_key}
+}
